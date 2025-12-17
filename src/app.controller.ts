2ec8f1c55506340e77c26cd743f4c135105c88cc
Damiano Panizzutti <dampaniz@amazon.lu>
2025-12-17 14:37:20 +0100

    Added Firebase middleware Integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { FirebaseGuard } from "@alpha018/nestjs-firebase-auth";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //1@UseGuards(FirebaseGuard) // This line protects your endpoint. If `validateRole` is enabled, it also validates the user's role.
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ExtractJwt } from "passport-jwt";
+
+import { FirebaseAdminModule } from "@alpha018/nestjs-firebase-auth";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    FirebaseAdminModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        base64: configService.get("FIREBASE_SERVICE_ACCOUNT_BASE64"),
+        options: {}, // Optionally, provide Firebase configuration here
+        auth: {
+          config: {
+            extractor: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extract JWT from the header
+            checkRevoked: true, // Optionally check if the token is revoked
+            validateRole: true, // Enable role validation if needed
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
